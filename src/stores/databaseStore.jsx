f76d083b3645e775e6813c9dc1fae97c8415fc37
Aurèle <97977554+AureleJ@users.noreply.github.com>
2025-04-22 22:50:47 +0200

    Update databaseStore.jsx

--- src/stores/databaseStore.jsx
+++ src/stores/databaseStore.jsx
@@ -1,8 +1,8 @@
 import { create } from 'zustand';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.env.local.SUPABASE_URL;
-const supabaseKey = import.env.local.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const useDatabaseStore = create((set, get) => ({
